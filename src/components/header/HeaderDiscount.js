340f4284d51720f1160a1ae95e5a837121d93bc8
nitesh-softions <nitesh.softieons@gmail.com>
2024-10-19 15:42:09 +0530

    - header marquee content changed

--- src/components/header/HeaderDiscount.js
+++ src/components/header/HeaderDiscount.js
@@ -5,21 +5,21 @@ const HeaderDiscount = () => {
   return (
     <div className="text-center offer-top discount-coupon">
         <div className="w-100 header-discount-content">
-            <marquee direction="right" loop="">
+            <marquee direction="left" loop="">
             <ul className="mb-0 d-flex align-items-center">
                 <li>
                   <div className='d-flex align-items-center'>
                     <BiSolidOffer className='fs-5 me-1 blink text-warning'/>
-                    <span className="offer">First order Discount: Flat 2%</span>
+                    <span className="offer">Experience ultimate relaxation with DR BWC: Pioneering wellness across India with cutting-edge massage chairs and holistic wellness solutions!</span>
                   </div>
                 </li>
-                <li className="ps-3 pe-3 text-white">|</li>
-                <li>
+                {/* <li className="ps-3 pe-3 text-white">|</li> */}
+                {/* <li>
                   <div className='d-flex align-items-center'>
                     <BiSolidOffer className='fs-5 me-1 blink text-warning'/>
                     <span className="offer">Deal of the day coupon: Flat 50</span>
                   </div>
-                </li>
+                </li> */}
             </ul>
             </marquee>
         </div>
